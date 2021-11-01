ecefd33a76a799afbd2fcd0d2dbb97fd03b567fd
yejin <banana2990@outlook.com>
2021-11-01 23:44:21 +0900

    Question #10171 고양이

---
+++ solved/Main.java
@@ -0,0 +1,15 @@
+package solved;
+
+public class Question_10171
+{
+    public static void main(String[] args)
+    {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
+// Question #10171
+// 고양이
+// 정규표현식 관련 문제
