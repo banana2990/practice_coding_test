c584395b5054647257da21b4466c175b12991c85
yejin <banana2990@outlook.com>
2021-11-05 23:05:51 +0900

    Question #10172 개

---
+++ solved/Question10172.java
@@ -0,0 +1,16 @@
+public class Question10172 {
+
+    public static void main(String[] args)
+    {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
+// Question #10172
+// 개
+// 정규표현식 관련 문제
+// > idea에서 작성할 경우 자동 수정해줘서 잘 모를 수 있지만, \나 "를 사용할 때에는 \를 같이 써줘야 함
+// > 컴파일 언어라서 \ 단독 사용시 정규식으로 인식해서 발생 (\r, \n 같이)
